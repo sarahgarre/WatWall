81557b46454a14ae057312b0d19bc34c22282357
Christophe Dupriez <dupriez@destin.be>
2020-02-05 10:56:47 +0100

    Update wat0.py

--- gw0/wat0.py
+++ gw0/wat0.py
@@ -1,26 +1,20 @@
 import socket
 import os
 
-# Run only ONCE: Check if /run/akuino/ELSA.pid exists...
-# pid = str(os.getpid())
-# self.pidfile = self.HardConfig.RUNdirectory+"/ELSA.pid"
-##
-# if os.path.isfile(self.pidfile):
-# print "%s already exists, exiting" % self.pidfile
-# sys.exit()
-# file(self.pidfile, 'w').write(pid)
-# Without holding a reference to our socket somewhere it gets garbage
-# collected when the function exits
+DIR_BASE = os.path.normpath("~")
+if not os.path.samefile(os.getcwd(), DIR_BASE):
+     os.chdir(DIR_BASE)
+print os.getcwd()
+
+pid = str(os.getpid())
 _lock_socket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
 
 try:
      _lock_socket.bind('\0GW0')
-     print ('Socket GW0 now locked')
+     print ('Socket GW0 now locked for process #'+pid)
+     file("pid.txt", 'w').write(pid)
 except socket.error:
-     print ('GW0 lock exists')
+     current = file("pid.txt", 'r').read()
+     print ('GW0 lock exists for process #'+current+" : may be you should kill it!")
      sys.exit()
 
-if not os.path.samefile(os.getcwd(), DIR_BASE):
-     os.chdir(DIR_BASE)
-
-print os.getcwd()
