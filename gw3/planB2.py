1f261ac27ce08ea7db6f4ec2eac4dd7e24e064a2
Matthieu Delespesse <matthieu.delespesse@student.uliege.be>
2020-04-18 16:26:54 +0200

    advancement

--- gw3/planB2.py
+++ gw3/planB2.py
@@ -1,15 +1,49 @@
-import os # operating system
 import csv # module for csv files
 import time
+import os,sys
+import socket
+
 
 # Parameters
 A=1920           # box area [cm2]
 Q=1000           # discharge [cm3/hr]
 Kc=1             # cultural coefficient [-]
 
+user = "GW3"
+test = False
+# True to run the code locally
+# False to implement the code on the server
 
-print(os.getcwd())                                          # c=current, w=working, d=directory
-os.chdir("C:\Users\pbray\PycharmProjects\WatWall2\gw3")     # from root
+# 1) Ensure to run in the user home directory
+# !!! MUST NOT BE CHANGED !!!
+
+if test:
+    host = "greenwall.gembloux.uliege.be"
+else:
+    host = "localhost"
+    # Ensure to run in the user home directory
+    DIR_BASE = os.path.expanduser("~")
+    if not os.path.samefile(os.getcwd(), DIR_BASE):
+        os.chdir(DIR_BASE)
+    print(os.getcwd())
+
+# 2)Ensure to be the only instance to run
+# !!! MUST NOT BE CHANGED !!!
+# Explanation: if another program is running, it gets killed and replaced by this one
+
+    pid = str(os.getpid())
+    _lock_socket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+
+    try:
+        _lock_socket.bind('\0'+user)
+        print('Socket '+user+' now locked for process #' + pid)
+        # Make the current pid available to be able to kill the process...
+        open("pid.txt", 'w').write(pid)
+    except socket.error:
+        current = open("pid.txt", 'r').read()
+        print(user+' lock exists for process #' + current + " : may be you should ./clean.sh !")
+        sys.exit()
+
 
 # ET file of Gembloux
 file=open("ET0_2010_2019.csv","r")                          # open the file
