ef19e0d4f31669d81d6bb07ba0454a7c6210ac8f
RaphaelPecqueux <raphael.pecqueux@student.uclouvain.be>
2020-05-01 17:42:40 +0200

    Ajout de messages de suivi des réalisations du code

--- gw1/wat1.py
+++ gw1/wat1.py
@@ -100,16 +100,29 @@ while (True):
         del humidite[0][2]
         t+=2
 
+    # Permet d'afficher le nombre de données supprimées
+    if t==3: # correspond à l'effacement de 2 données
+        z=2
+    elif t==2: # correspond à l'effacement du max mais ce n'est qu'une donnée
+        z=1
+    else:
+        z=t # sinon le t correspond au nombre de données supprimées
+    print("Pour les capteurs d'humidités,"+str(z)+" "+"données ont été supprimées")
+
     # Combien de valeurs reste-t-il ?
     if t!=3:
         moyenne_humidite=[sum(humidite[0])/len(humidite[0])]
+        print("On effectue le plan A et la teneur en eau moyenne est de"+" "+str(moyenne_humidite[0]))
 
         # Où se situe l'humidité moyenne ?
         if moyenne_humidite[0]>0.285: # regarde si elle est supérieure à la CC
             V_irrigation=0 # y'a assez d'eau on n'irrigue pas
+            print("Irriguer n'est pas nécessaire, on est au dessus de la CC")
         else:
             V_irrigation = (0.285 - moyenne_humidite[0]) * 12.6 # volume d'irrigation nécessaire pour atteindre la CC
+            print("On va irriguer"+" "+str(V_irrigation)+" litres pour atteindre la CC")
     else:
+        print("Trop de problèmes avec nos valeurs des capteurs d'humidité, on passe au plan B")
         # Receuil des données météo des 24 dernières heures et nécessaires au calcul de l'ETP
         dataFile = None
         meteo = [[] for i in range(6)]  # tableau permettant le stockage des valeurs receuillies
@@ -167,23 +180,32 @@ while (True):
             gamma = 0.665 * meteo[4][q] * 10 ** (-3)
             vitesse_du_vent = meteo[1][q]
             ET0 += (0.408 * delta * Rn + gamma * (0.625 / (273 + meteo[2][q])) * vitesse_du_vent * (es - ea)) / (delta + gamma * (1 + 0.34 * vitesse_du_vent))  # stocke la somme des ET0 calculés pour chaque minute
+        print("L'ET0 calculé sur les 24 dernières heures est de"+" "+str (ET0)+" "+"mm")
 
         if 0<ET0<8:
+            print("On effectue donc le plan B")
             ETR = ET0 * Kc  # valeur réelle de l'ETP en considérant le type et le stade de la culture
             V_irrigation = ETR * 10 ** (-2) * 10.5 # volume qui a été perdu par évapotranspiration
             moyenne_humidite[0]= humidite[0]
+            print("On va donc irriguer"+" "+str(V_irrigation)+" "+"litres pour compenser l'ETP des 24 dernières heures")
+            print("La teneur en eau moyenne est de"+" "+str(moyenne_humidite[0]))
         else:
+            print("On effectue donc le plan C")
             ET0=float(open("../WatWall/gw1/ET0.csv", 'r').read().split("\n")[J - 1]) # trouve la valeur moyenne d'ET0 pour aujourd'hui dans notre fichier
+            print("On prend donc"+" "+str(ET0)+"comme valeur d'ET0 à la place")
             ETR = ET0 * Kc
             V_irrigation = ETR * 10 ** (-2) * 10.5
             moyenne_humidite[0] = humidite[0]
 
     # Le volume d'eau est-il suffisant ?
     if V_irrigation < 0.25:  # regarde si le volume à irriguer est assez important, on fait cela à cause de l'incertitude de précision d'arrosage des petits volumes
+        print("Le volume d'eau à irriguer est insuffisant, aucune irrigation n'est appliquée ")
         time.sleep(24 * 60 * 60)  # ce n'est pas le cas donc on irrigue pas et on attend le jour suivant
+        sys.stdout.flush() # permet de regarder aux messages
     else:
         # Planning d'irrigation
         temps_irrigation = round(V_irrigation / 0.000416)  # calcul le temps correspondant au volume précédemment calculé
+        print("On va donc ouvrir les vannes pendant"+" "+str(temps_irrigation)+" "+"secondes soit environ"+" "+str(round(temps_irrigation/60))+" "+"minutes")
         timestamp = get_timestamp()
         n = 0
         if temps_irrigation <= 1200:
@@ -201,44 +223,52 @@ while (True):
                 n += 1
             open("valve.txt", 'a').write(str(timestamp + n * 3600) + ";1\n")
             open("valve.txt", 'a').write(str(timestamp + n * 3600 + temps_irrigation) + ";0\n")
+        if n==0:
+            print("On peut donc uniquement irriguer sur l'heure actuelle")
+        else:
+            print("On va donc irriguer sur"+" "+str(n+1)+" "+"heures différentes")
         time.sleep(4*60*60) # fait une pause dans l'éxécution de 4h pour que l'eau atteigne les capteurs d'humidité
+        sys.stdout.flush()
 
-    # Vérification pour voir si l'irrigation a bien été effectuée
+        # Vérification pour voir si l'irrigation a bien été effectuée
 
-    # Recueil des nouvelles dernières valeurs de tension des capteurs d'humidité
-    dataFile = None
-    for g in range(1, 4):  # boucle collectant les 3 dernières valeurs de nos capteurs d'humidité
-        try:  # urlopen not usable with "with"
-            url = "http://" + host + "/api/get/%21s_HUM" + unicode(g)
-            dataFile = urllib.urlopen(url, None, 20)
-            data = dataFile.read(80000)
-            humidite[1].append((float(data.strip(delimiters))))  # ajout de la valeur receuillie en fin de liste
-        except:
-            print(u"URL=" + (url if url else "") + \
-                    u", Message=" + traceback.format_exc())
-        if dataFile:
-            dataFile.close()
+        # Recueil des nouvelles dernières valeurs de tension des capteurs d'humidité
+        dataFile = None
+        for g in range(1, 4):  # boucle collectant les 3 dernières valeurs de nos capteurs d'humidité
+            try:  # urlopen not usable with "with"
+                url = "http://" + host + "/api/get/%21s_HUM" + unicode(g)
+                dataFile = urllib.urlopen(url, None, 20)
+                data = dataFile.read(80000)
+                humidite[1].append((float(data.strip(delimiters))))  # ajout de la valeur receuillie en fin de liste
+            except:
+                print(u"URL=" + (url if url else "") + \
+                        u", Message=" + traceback.format_exc())
+            if dataFile:
+                dataFile.close()
 
-    # Conversion des tensions en teneur en eau
-    for o in range(0, 3):
-        humidite[1][o] = ((35.24 * humidite[1][o] - 15.44) / (humidite[1][o] - 0.3747)) / 100
-    humidite[1].sort()
+        # Conversion des tensions en teneur en eau
+        for o in range(0, 3):
+            humidite[1][o] = ((35.24 * humidite[1][o] - 15.44) / (humidite[1][o] - 0.3747)) / 100
+        humidite[1].sort()
 
-    # Elimination des mêmes valeurs que la première fois et calcul de la nouvelle humidité moyenne
-    if t==1:
-        del humidite[1][0]
-    elif t==2:
-        del humidite[1][2]
-    elif t==3:
-        del humidite[1][0]
-        del humidite[1][2]
-    moyenne_humidite.append(sum(humidite[1]) / len(humidite[1]))
+        # Elimination des mêmes valeurs que la première fois et calcul de la nouvelle humidité moyenne
+        if t==1:
+            del humidite[1][0]
+        elif t==2:
+            del humidite[1][2]
+        elif t==3:
+            del humidite[1][0]
+            del humidite[1][2]
+        moyenne_humidite.append(sum(humidite[1]) / len(humidite[1]))
+        print("La nouvelle teneur en eau moyenne est de"+" "+str(moyenne_humidite[1]))
 
-    # Vérification de l'augmentation de l'humidité moyenne
-    if moyenne_humidite[1] - moyenne_humidite[0] > 0:  # regarde si la différence d'humidité moyenne est positive, preuve qu'elle a bien eu lieu
-        time.sleep(20*60*60)  # fait une pause de 20h dans l'éxécution
-
-    # si celle-ci n'a pas augmenté le programme recommence depuis le début des calculs
+        # Vérification de l'augmentation de l'humidité moyenne
+        if moyenne_humidite[1] - moyenne_humidite[0] > 0:  # regarde si la différence d'humidité moyenne est positive, preuve qu'elle a bien eu lieu
+            print("C'est donc plus élevé que 4 heures plus tôt, l'irrigation a fonctionné :)")
+            time.sleep(20*60*60)  # fait une pause de 20h dans l'éxécution
+            sys.stdout.flush()  # permet de regarder aux messages
+        else:
+            print("Aïe l'irrigation n'a pas fonctionné, bon bah on recommence:(") # si celle-ci n'a pas augmenté le programme recommence depuis le début des calculs
 
 
 
