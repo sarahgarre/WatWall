7375451b7542195fe2c6427c6447cd57a5141ac8
RaphaelPecqueux <raphael.pecqueux@student.uclouvain.be>
2020-05-12 10:08:15 +0200

    Faire les graphs

--- gw1/Graphiques/Création.py
+++ gw1/Graphiques/Création.py
@@ -67,3 +67,32 @@ except:
           u", Message=" + traceback.format_exc())
 if dataFile:
     dataFile.close()
+
+# Collecte des données d'humidité et de pluie sur le dernier mois
+
+dataFile = None
+Emplacement=["HUM1.csv","HUM2.csv","HUM3.csv","Pluie.csv"] # donne dans quel fichier csv les données doivent être ajoutées
+p=0 # Compteur qui permet de passer au fichier csv suivant
+for k in range(0,4): # initialise les fichiers csv comme étant initialement vides
+    open(Emplacement[k],'w').write("")
+try:  # urlopen not usable with "with"
+    url = "http://" + host + "/api/grafana/query"
+    now = get_timestamp()
+    gr = {'range': {'from': formatDateGMT(now - (30*24 * 60 * 60)), 'to': formatDateGMT(now)}, \
+          'targets': [{'target': 'HUM1'}, {'target': 'HUM2'}, {'target': 'HUM3'}, {'target': 'SDI1'}]}
+    data = json.dumps(gr)
+    dataFile = urllib.urlopen(url, data, 20)
+    result = json.load(dataFile)
+    if result:
+        for target in result:
+            index = target.get('target')
+            for datapoint in target.get('datapoints'):
+                value = datapoint[0]
+                stamp = datapoint[1] / 1000
+                open(Emplacement[p],'a').write(value)
+            p+=1
+except:
+    print(u"URL=" + (url if url else "") + \
+          u", Message=" + traceback.format_exc())
+if dataFile:
+    dataFile.close()
